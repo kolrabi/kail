1644a0dbf295cffa3efbe2f9676a1faac91c6ad2
Björn Paetzel <kolrabi@kolrabi.de>
2014-06-30 17:52:58 +0200

    Fixed PXR export.

--- src/IL/formats/il_pxr.c
+++ src/IL/formats/il_pxr.c
@@ -19,140 +19,155 @@
 #include "pack_push.h"
 typedef struct PIXHEAD
 {
-	ILubyte  	Signature[2];			// 0000
-	ILubyte		Reserved1[414];		// 0002
-	ILushort	Height;						// 01A0 
-	ILushort	Width;						// 01A2
-	ILushort	Height2;					// 01A4
-	ILushort	Width2;						// 01A6
-	ILubyte		Bpp;
-	//ILubyte		Reserved3[598];
+  ILubyte   Signature[2];     // 0000
+  ILubyte   Reserved1[414];   // 0002
+  ILushort  Height;           // 01A0 
+  ILushort  Width;            // 01A2
+  ILushort  Height2;          // 01A4
+  ILushort  Width2;           // 01A6
+  ILubyte   Bpp;              // 01A8
+  ILuint    Offset;           // 01A9 pointing to Offset2
+  ILushort  Unknown;          // 01AD
+  ILubyte   Reserved2[1+80];  // 01AF
+  ILuint    Offset2;          // 0200
+  ILuint    Size;
+
+  //ILubyte   Reserved3[592];
 } PIXHEAD;
 #include "pack_pop.h"
 
 static ILboolean iIsValidPxr(SIO *io) {
-	ILuint 	Start = SIOtell(io);
-	PIXHEAD	Head;
-	ILuint  Read = SIOread(io, &Head, 1, sizeof(Head));
-	SIOseek(io, Start, IL_SEEK_SET);
+  ILuint  Start = SIOtell(io);
+  PIXHEAD Head;
+  ILuint  Read = SIOread(io, &Head, 1, sizeof(Head));
+  SIOseek(io, Start, IL_SEEK_SET);
 
-	return Read == sizeof(Head) && !memcmp(Head.Signature, "\x80\xe8", 2);
+  return Read == sizeof(Head) && !memcmp(Head.Signature, "\x80\xe8", 2);
 }
 
 // Internal function used to load the Pxr.
 static ILboolean iLoadPxrInternal(ILimage *Image) {
-	PIXHEAD		Head;
-	SIO *     io;
+  PIXHEAD   Head;
+  SIO *     io;
 
     if (Image == NULL) {
-		iSetError(IL_ILLEGAL_OPERATION);
-		return IL_FALSE;
-	}
+    iSetError(IL_ILLEGAL_OPERATION);
+    return IL_FALSE;
+  }
 
-	io = &Image->io;
+  io = &Image->io;
 
-	if (SIOread(io, &Head, 1, sizeof(Head)) != sizeof(Head))
-		return IL_FALSE;
+  if (SIOread(io, &Head, 1, sizeof(Head)) != sizeof(Head))
+    return IL_FALSE;
 
-	Head.Reserved1[2] = 1;
+  UShort(&Head.Height);
+  UShort(&Head.Width);
 
-	UShort(&Head.Height);
-	UShort(&Head.Width);
+  switch (Head.Bpp) {
+    case 0x08:
+      iTexImage(Image, Head.Width, Head.Height, 1, 1, IL_LUMINANCE, IL_UNSIGNED_BYTE, NULL);
+      break;
+    case 0x0E:
+      iTexImage(Image, Head.Width, Head.Height, 1, 3, IL_RGB, IL_UNSIGNED_BYTE, NULL);
+      break;
+    case 0x0F:
+      iTexImage(Image, Head.Width, Head.Height, 1, 4, IL_RGBA, IL_UNSIGNED_BYTE, NULL);
+      break;
+    default:
+      iSetError(IL_INVALID_FILE_HEADER);
+      return IL_FALSE;
+  }
 
-	switch (Head.Bpp) {
-		case 0x08:
-			iTexImage(Image, Head.Width, Head.Height, 1, 1, IL_LUMINANCE, IL_UNSIGNED_BYTE, NULL);
-			break;
-		case 0x0E:
-			iTexImage(Image, Head.Width, Head.Height, 1, 3, IL_RGB, IL_UNSIGNED_BYTE, NULL);
-			break;
-		case 0x0F:
-			iTexImage(Image, Head.Width, Head.Height, 1, 4, IL_RGBA, IL_UNSIGNED_BYTE, NULL);
-			break;
-		default:
-			iSetError(IL_INVALID_FILE_HEADER);
-			return IL_FALSE;
-	}
+  SIOseek(io, 1024 - sizeof(Head), IL_SEEK_CUR);
+  if (SIOread(io, Image->Data, 1, Image->SizeOfData) != Image->SizeOfData) 
+    return IL_FALSE;
 
-	SIOseek(io, 1024 - sizeof(Head), IL_SEEK_CUR);
-	if (SIOread(io, Image->Data, 1, Image->SizeOfData) != Image->SizeOfData) 
-		return IL_FALSE;
-
-	Image->Origin = IL_ORIGIN_UPPER_LEFT;
-	return IL_TRUE;
+  Image->Origin = IL_ORIGIN_UPPER_LEFT;
+  return IL_TRUE;
 }
 
 static ILboolean iSavePxrInternal(ILimage *Image) {
-	PIXHEAD		Head;
-	SIO *     io;
-	ILimage * TempImage = Image;
-	ILuint    Written;
+  PIXHEAD   Head;
+  SIO *     io;
+  ILimage * TempImage = Image;
+  ILuint    Written;
 
   if (Image == NULL) {
-		iSetError(IL_ILLEGAL_OPERATION);
-		return IL_FALSE;
-	}
+    iSetError(IL_ILLEGAL_OPERATION);
+    return IL_FALSE;
+  }
 
-	io = &Image->io;
+  io = &Image->io;
 
-	if (Image->Type != IL_UNSIGNED_BYTE) {
-		TempImage = iConvertImage(Image, IL_RGB, IL_UNSIGNED_BYTE);
-	}
+  if (Image->Type != IL_UNSIGNED_BYTE) {
+    TempImage = iConvertImage(Image, IL_RGB, IL_UNSIGNED_BYTE);
+  }
 
-	imemclear(&Head, sizeof(Head));
-	Head.Signature[0] = 0x80;
-	Head.Signature[1] = 0xe8;
-	Head.Height       = TempImage->Height;
-	Head.Width        = TempImage->Width;
-	Head.Height2      = TempImage->Height;
-	Head.Width2       = TempImage->Width;
+  imemclear(&Head, sizeof(Head));
+  Head.Signature[0] = 0x80;
+  Head.Signature[1] = 0xe8;
+  Head.Height       = TempImage->Height;
+  Head.Width        = TempImage->Width;
+  Head.Height2      = TempImage->Height;
+  Head.Width2       = TempImage->Width;
+  Head.Reserved1[2] = 1;
 
-	switch (TempImage->Format) {
-		case IL_LUMINANCE: 	Head.Bpp = 0x08; break;
-		case IL_RGB: 				Head.Bpp = 0x0E; break;
-		case IL_RGBA: 			Head.Bpp = 0x0F; break;
-		default:
-			TempImage = iConvertImage(Image, IL_RGB, IL_UNSIGNED_BYTE);
-			Head.Bpp = 0x0E;
-	}
+  Head.Offset   = 512;
+  Head.Unknown  = 4;
+  Head.Offset2  = 1024;
 
-	if (TempImage->Origin != IL_ORIGIN_UPPER_LEFT) {
-		if (TempImage == Image) {
-			TempImage = ilCopyImage_(Image);
-		}
-		iFlipBuffer((ILubyte *)TempImage->Data, 1, TempImage->Bps, TempImage->Height);
-	}
+  switch (TempImage->Format) {
+    case IL_LUMINANCE:  Head.Bpp = 0x08; break;
+    case IL_RGB:        Head.Bpp = 0x0E; break;
+    case IL_RGBA:       Head.Bpp = 0x0F; break;
+    default:
+      TempImage = iConvertImage(Image, IL_RGB, IL_UNSIGNED_BYTE);
+      Head.Bpp = 0x0E;
+  }
 
-	UShort(&Head.Height);
-	UShort(&Head.Width);
-	UShort(&Head.Height2);
-	UShort(&Head.Width2);
+  if (TempImage->Origin != IL_ORIGIN_UPPER_LEFT) {
+    if (TempImage == Image) {
+      TempImage = ilCopyImage_(Image);
+    }
+    iFlipBuffer((ILubyte *)TempImage->Data, 1, TempImage->Bps, TempImage->Height);
+  }
 
-	Written = SIOwrite(io, &Head, 1, sizeof(Head));
-	if (Written != sizeof(Head)) {
-		iSetError(IL_FILE_WRITE_ERROR);
-		return IL_FALSE;
-	}
-	SIOpad(io, 1024 - Written);
+  Head.Size = TempImage->Height * TempImage->Width * ilGetBppFormat(TempImage->Format);
 
-	SIOwrite(io, TempImage->Data, ilGetBppFormat(TempImage->Format), Head.Width * Head.Height);
+  UShort(&Head.Height);
+  UShort(&Head.Width);
+  UShort(&Head.Height2);
+  UShort(&Head.Width2);
+  UInt  (&Head.Offset);
+  UShort(&Head.Unknown);
+  UInt  (&Head.Offset2);
+  UInt  (&Head.Size);
 
-	if (TempImage != Image)
-		iCloseImage(TempImage);
+  Written = SIOwrite(io, &Head, 1, sizeof(Head));
+  if (Written != sizeof(Head)) {
+    iSetError(IL_FILE_WRITE_ERROR);
+    return IL_FALSE;
+  }
+  SIOpad(io, 1024 - Written);
 
-	return IL_TRUE;
+  SIOwrite(io, TempImage->Data, ilGetBppFormat(TempImage->Format), Head.Width * Head.Height);
+
+  if (TempImage != Image)
+    iCloseImage(TempImage);
+
+  return IL_TRUE;
 }
 
 ILconst_string iFormatExtsPXR[] = { 
-	IL_TEXT("pxr"), 
-	NULL 
+  IL_TEXT("pxr"), 
+  NULL 
 };
 
 ILformat iFormatPXR = { 
-	/* .Validate = */ iIsValidPxr, 
-	/* .Load     = */ iLoadPxrInternal, 
-	/* .Save     = */ iSavePxrInternal, 
-	/* .Exts     = */ iFormatExtsPXR
+  /* .Validate = */ iIsValidPxr, 
+  /* .Load     = */ iLoadPxrInternal, 
+  /* .Save     = */ iSavePxrInternal, 
+  /* .Exts     = */ iFormatExtsPXR
 };
 
 #endif//IL_NO_PXR
